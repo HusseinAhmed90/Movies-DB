48d51e848ec7cf83f3aa846f37228463dc079ed1
Hussein <hussein.saeed90@outlook.com>
2022-01-16 04:12:36 +0200

    Adding fragments and setting up the navigation

---
+++ app/src/main/java/com/example/moviesdb/views/MovieDetailsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.moviesdb.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.moviesdb.R
+
+class MovieDetailsFragment : Fragment() {
+
+    private val args: MovieDetailsFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO: show movie details
+        val movie = args.movie
+    }
+
+}
